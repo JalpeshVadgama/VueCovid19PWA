103083cd4f7cd3d1fa32b0d293691e206cd620a7
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2020-04-18 00:30:30 +0530

    Added Cards in home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>Home component loaded</div>
+  <div>
+    <md-card class="red" md-with-hover>
+      <md-ripple>
+        <md-card-header>
+          <div class="md-title">Total confirmed cases</div>
+        </md-card-header>
+        <md-card-content></md-card-content>
+      </md-ripple>
+    </md-card>
+    <md-card class="blue" md-with-hover>
+      <md-ripple>
+        <md-card-header>
+          <div class="md-title">Total active cases</div>
+        </md-card-header>
+
+        <md-card-content></md-card-content>
+      </md-ripple>
+    </md-card>
+    <md-card class="green" md-with-hover>
+      <md-ripple>
+        <md-card-header>
+          <div class="md-title">Total death</div>
+        </md-card-header>
+
+        <md-card-content></md-card-content>
+      </md-ripple>
+    </md-card>
+    <md-card class="grey" md-with-hover>
+      <md-ripple>
+        <md-card-header>
+          <div class="md-title">Total recovered</div>
+        </md-card-header>
+
+        <md-card-content></md-card-content>
+      </md-ripple>
+    </md-card>
+  </div>
 </template>
 
 <script>
@@ -13,4 +49,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.md-card {
+  width: 300px;
+  margin: 4px;
+  display: inline-block;
+  vertical-align: top;
+  color: white !important;
+}
+.red {
+  background: #ff073a !important;
+}
+.blue {
+  background: #007bff !important;
+}
+.green {
+  background: rgba(40, 167, 69, 0.6) !important;
+}
+.grey {
+  background: #6c757d !important;
+}
 </style>
