9c689378e2ad4b3ebaeda923f2f16ad2ec2f2199
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2020-04-21 01:20:44 +0530

    data with colors

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="padding-top:20px !important;">
     <div style="text-align: center;">
       <md-card class="red" md-with-hover>
         <md-ripple>
@@ -43,29 +43,38 @@
         </md-ripple>
       </md-card>
     </div>
-    <div></div>
-    <div style="text-align: center;width:1200px !important;">
-      <div v-if="this.statecases">
-        <md-table
-          v-model="this.statecases"
-          md-sort="state"
-          md-sort-order="asc"
-          md-card
-          md-fixed-header
-        >
-          <md-table-toolbar>
-            <h1 class="md-title">Statewise Cases</h1>
-          </md-table-toolbar>
+    <div>&nbsp;</div>
+    <div>&nbsp;</div>
+    <div>&nbsp;</div>
+    <div>&nbsp;</div>
+    <div v-if="this.statecases" style="text-align: center;">
+      <md-table
+        v-model="this.statecases"
+        md-sort="state"
+        md-sort-order="asc"
+        md-card
+        md-fixed-header
+      >
+        <md-table-toolbar>
+          <h1 class="md-title">Statewise Cases</h1>
+        </md-table-toolbar>
 
-          <md-table-row slot="md-table-row" slot-scope="{ item }">
-            <md-table-cell md-label="State">{{ item.state }}</md-table-cell>
-            <md-table-cell md-label="Confirmed" md-numeric>{{ item.confirmed }}</md-table-cell>
-            <md-table-cell md-label="Active" md-numeric>{{ item.active }}</md-table-cell>
-            <md-table-cell md-label="Recovered" md-numeric>{{ item.recovered }}</md-table-cell>
-            <md-table-cell md-label="Death" md-numeric>{{ item.deaths }}</md-table-cell>
-          </md-table-row>
-        </md-table>
-      </div>
+        <md-table-row slot="md-table-row" slot-scope="{ item }">
+          <md-table-cell md-label="State" md-text>
+            <strong>{{ item.state }}</strong>
+          </md-table-cell>
+          <md-table-cell md-label="Confirmed" md-numeric>
+            <span style="color:#ff073a">{{ item.confirmed }}</span>
+          </md-table-cell>
+          <md-table-cell md-label="Active" md-numeric>
+            <span style="color:#0091ffe8">{{ item.active }}</span>
+          </md-table-cell>
+          <md-table-cell md-label="Recovered" md-numeric>
+            <span style="color:rgba(40, 167, 69, 0.6)">{{ item.recovered }}</span>
+          </md-table-cell>
+          <md-table-cell md-label="Death" md-numeric>{{ item.deaths }}</md-table-cell>
+        </md-table-row>
+      </md-table>
     </div>
   </div>
 </template>
@@ -83,12 +92,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.md-table {
+  width: 94% !important;
+}
 .md-card {
-  width: 300px;
+  width: 22%;
   margin: 4px;
   display: inline-block;
   vertical-align: top;
   color: white !important;
+  min-width: 300px;
 }
 .md-card-content {
   font-size: 20px !important;
